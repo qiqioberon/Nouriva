fc32cb4f4f5840e5a59c786790a449cea5d28afa
qiqioberon <aquq1q1.farrukh@gmail.com>
2024-04-27 21:08:43 +0700

    fix: add use client di nutrition repot

--- src/app/(Home)/NutritionReport/page.tsx
+++ src/app/(Home)/NutritionReport/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 import { cn } from "@/lib/utils";
-
 import { Inter, Source_Serif_4 } from "next/font/google";
 import Image from "next/image";
 import { PieChart } from "@/components/Home/Piechart/piechar";
