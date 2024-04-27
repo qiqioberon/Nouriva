1ca74118b370b28a8c7d5b7968222e13d6da66ad
qiqioberon <aquq1q1.farrukh@gmail.com>
2024-04-27 18:18:46 +0700

    fix: add query provider and toast container on home

--- src/app/(Home)/layout.tsx
+++ src/app/(Home)/layout.tsx
@@ -1,15 +1,20 @@
 import Navbar from "@/components/Home/navbar";
-
+import { QueryProvider } from "@/components/providers/query-provider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function Home({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<>
-			<Navbar />
+			<QueryProvider>
+				<Navbar />
 
-			<div className="pt-24 flex justify-center items-center w-full">
-				{children}
-			</div>
+				<div className="pt-24 flex justify-center items-center w-full">
+					{children}
+				</div>
+				<ToastContainer position="top-center" />
+			</QueryProvider>
 		</>
 	);
 }
