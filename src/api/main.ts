48dec7bf37dac08a9921e9649576b5736c54a4ff
qiqioberon <aquq1q1.farrukh@gmail.com>
2024-04-27 21:38:29 +0700

    fix: coba lagi

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,26 +1,22 @@
-import useUserStore from '@/store/userstore';
-import axios from 'axios';
+import axios from "axios";
+import useUserStore from "../store/userstore";
 
+export const baseURL = process.env.API_URL;
 
 const main = axios.create({
-    baseURL: 'https://api.yourservice.com',
+    baseURL: "https://nouriva-backend.unedotampubolon.codes/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: false,
 });
 
 main.interceptors.request.use((config) => {
-    let accessToken = null;
-
-    if (typeof window !== 'undefined') {
-        accessToken =
-            useUserStore.getState().accessToken || localStorage.getItem('accessToken');
-    } else {
-        accessToken = useUserStore.getState().accessToken;
-    }
-
+    const accessToken =
+        useUserStore.getState().accessToken || localStorage.getItem("accessToken");
     if (accessToken) {
         config.headers.Authorization = `Bearer ${accessToken}`;
     }
-
     return config;
 });
-
 export default main;
