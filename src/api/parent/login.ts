0343694ae0d4742ae1107d7c7ddd8dbee46235c5
qiqioberon <aquq1q1.farrukh@gmail.com>
2024-04-27 15:09:04 +0700

    feat: add api for login and register parent

---
+++ src/api/parent/login.ts
@@ -0,0 +1,45 @@
+import useUserStore from '@/store/userstore';
+import { useMutation, MutationFunction } from "@tanstack/react-query";
+import main from "../main";
+import { LoginUser as LoginUserType } from "@/types/LoginUserType";
+import { toast } from "react-toastify";
+
+
+
+export default function LoginUser() {
+    const {
+        mutate: mutateUseLoginUser,
+        data: response,
+        isSuccess,
+    } = useMutation({
+        mutationFn: (data: LoginUserType) => {
+            const loginUser = data;
+            return main.post(`auth/login`, loginUser);
+        },
+        onError: (error: Error) => toast.error(getErrorMessage(error)),
+        onSuccess: () => toast.success("Sukses Login"),
+    });
+
+    if (response && response.data && response.data.accessToken) {
+        const { accessToken } = response.data;
+        useUserStore.getState().setAccessToken(accessToken);
+        localStorage.setItem("accessToken", accessToken);
+        console.log(useUserStore.getState().accessToken);
+    }
+
+
+    return { mutateUseLoginUser, response, isSuccess };
+}
+
+
+function getErrorMessage(error: any) {
+    let resultMessage = "";
+    if (error) {
+        console.log(error.request);
+        const responseJson = JSON.parse(error.request.response);
+        console.log(responseJson.resultMessage.en);
+
+        resultMessage = responseJson.resultMessage.en.toString();
+    }
+    return resultMessage;
+}
---
+++ src/api/parent/register.ts
@@ -0,0 +1,34 @@
+import { useMutation, MutationFunction } from "@tanstack/react-query";
+import main from "../main";
+import { Register as SignUpForm } from "@/types/RegisterType";
+import { toast } from "react-toastify";
+
+export default function SignUpUser() {
+    const {
+        mutate: mutateSignUp,
+        data: responseSignUp,
+        isSuccess,
+        error,
+    } = useMutation({
+        mutationFn: (data: SignUpForm) => {
+            const SignUpUser = data;
+            return main.post(`parents/register`, SignUpUser);
+        },
+        onError: (error: any) => toast.error(getErrorMessage(error)),
+        onSuccess: () => toast.success("Sukses Sign Up"),
+    });
+
+    return { mutateSignUp, responseSignUp, isSuccess, error };
+}
+
+function getErrorMessage(error: any) {
+    let resultMessage = "";
+    if (error) {
+        console.log(error.request);
+        const responseJson = JSON.parse(error.request.response);
+        console.log(responseJson.resultMessage.en);
+
+        resultMessage = responseJson.resultMessage.en.toString();
+    }
+    return resultMessage;
+}
